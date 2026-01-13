2f13806852225e9b89cf4bfb2ac0f608dc1d1810
Yaroslav Olieniev <olieniev.od.ua@gmail.com>
2026-01-13 17:27:08 +0100

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,65 @@
 'use strict';
+
+const firstPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+
+  document.addEventListener('click', () => {
+    leftClick = true;
+    resolve('First promise was resolved');
+  });
+
+  setTimeout(() => {
+    if (!leftClick) {
+      reject(new Error('First promise was rejected'));
+    }
+  }, 3000);
+});
+
+const secondPromise = new Promise((resolve) => {
+  document.addEventListener('click', () => {
+    resolve('Second promise was resolved');
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    resolve('Second promise was resolved');
+  });
+});
+
+const thirdPromise = new Promise((resolve, reject) => {
+  let leftClick = false;
+  let rightClick = false;
+
+  document.addEventListener('click', () => {
+    leftClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    rightClick = true;
+
+    if (leftClick && rightClick) {
+      resolve('Third promise was resolved');
+    }
+  });
+});
+
+function showNotification(message, isSuccess) {
+  const div = document.createElement('div');
+
+  div.setAttribute('data-qa', 'notification');
+  div.className = isSuccess ? 'success' : 'error';
+  div.textContent = message;
+  document.body.append(div);
+}
+
+firstPromise
+  .then((message) => showNotification(message, true))
+  .catch((error) => showNotification(error.message, false));
+
+secondPromise.then((message) => showNotification(message, true));
+thirdPromise.then((message) => showNotification(message, true));
