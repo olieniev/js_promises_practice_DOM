25619d77584a02dc5516c0f296a5c4e5d98df7fb
Yaroslav Olieniev <olieniev.od.ua@gmail.com>
2026-01-14 10:33:09 +0100

    improved solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,40 +3,40 @@
 const firstPromise = new Promise((resolve, reject) => {
   let leftClick = false;
 
-  document.addEventListener(
-    'click',
-    () => {
-      leftClick = true;
-      resolve('First promise was resolved');
-      clearTimeout(timeout);
-    },
-    { once: true },
-  );
+  const handler = () => {
+    leftClick = true;
+    resolve('First promise was resolved');
+    clearTimeout(timeout);
+  };
+
+  document.addEventListener('click', handler, { once: true });
 
   const timeout = setTimeout(() => {
     if (!leftClick) {
+      document.removeEventListener('click', handler);
       reject(new Error('First promise was rejected'));
     }
   }, 3000);
 });
 
 const secondPromise = new Promise((resolve) => {
-  document.addEventListener(
-    'click',
-    () => {
-      resolve('Second promise was resolved');
-    },
-    { once: true },
-  );
+  const cleanup = () => {
+    document.removeEventListener('click', leftClickHandler);
+    document.removeEventListener('contextmenu', rightClickHandler);
+  };
+  const leftClickHandler = () => {
+    cleanup();
+    resolve('Second promise was resolved');
+  };
+  const rightClickHandler = (e) => {
+    cleanup();
+    e.preventDefault();
+    resolve('Second promise was resolved');
+  };
 
-  document.addEventListener(
-    'contextmenu',
-    (e) => {
-      e.preventDefault();
-      resolve('Second promise was resolved');
-    },
-    { once: true },
-  );
+  document.addEventListener('click', leftClickHandler, { once: true });
+
+  document.addEventListener('contextmenu', rightClickHandler, { once: true });
 });
 
 const thirdPromise = new Promise((resolve, reject) => {
